0fb87dd6ea4097c93a77251b6e931cf16a4a71ac
Ricardo Siena <ricardo.siena@alunoescolassociais.org.br>
2024-11-01 11:07:03 -0300

    Adiciona arquivos prontos de HTML e CSS e

---
+++ CON-API.js
@@ -0,0 +1,5 @@
+// Armazena chave que foi dada pelo site após criarmos uma conta
+const apiKey = 'd5e06d9f3591549b1247ec72';
+
+// Armazena URL que foi dada pelo site junto da chave anterior
+ const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
