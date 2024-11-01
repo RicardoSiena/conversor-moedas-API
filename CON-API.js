c6ba4c90b80e450cf485c33d38a1859f574ca255
Ricardo Siena <ricardo.siena@alunoescolassociais.org.br>
2024-11-01 11:10:13 -0300

    Adiciona arquivo CSS, HTML e inicia arquivo README

--- CON-API.js
+++ CON-API.js
@@ -3,3 +3,5 @@ const apiKey = 'd5e06d9f3591549b1247ec72';
 
 // Armazena URL que foi dada pelo site junto da chave anterior
  const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
+
+ // Função para buscar taxa de câmbio da API
